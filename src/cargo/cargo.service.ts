5a293445165ac190f0d6b31c7df65c578ee7647c
Piero Muñante Avila <pmunanteavila@gmail.com>
2025-06-21 17:24:33 -0500

    Actualización funcional del servicio

--- src/cargo/cargo.service.ts
+++ src/cargo/cargo.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Cargo } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CrearCargoInput } from './crear-cargo.input';
 
 @Injectable()
 export class CargoService {
@@ -34,4 +35,54 @@ export class CargoService {
     });
     return Number(resultado._sum.pagoHora) || 0;
   }
+
+  async obtenerCargoMejorPagado(): Promise<string | null> {
+    const resultado = await this.prisma.cargo.findFirst({
+      orderBy: {
+        pagoHora: 'desc',
+      },
+      select: {
+        cargo: true,
+      },
+    });
+    return resultado?.cargo || null;
+  }
+
+  async obtenerCargoPorNombre(cargo: string): Promise<Cargo | null> {
+    return this.prisma.cargo.findFirst({
+      where: {
+        cargo: {
+          equals: cargo,
+        },
+      },
+    });
+  }
+
+  async crearCargo(input: CrearCargoInput): Promise<void> {
+    const { cargo, pagoHora } = input;
+    const cargoExistente = await this.prisma.cargo.findFirst({
+      where: { cargo },
+    });
+    if(cargoExistente) {
+      throw new HttpException(
+        'El cargo ya existe en la base de datos',
+        HttpStatus.CONFLICT,
+      );
+    }
+    try {
+      await this.prisma.cargo.create({
+        data: {
+          cargo,
+          pagoHora,
+        },
+      });
+    } catch (error) {
+      throw new HttpException(
+        'Error al registrar el cargo',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  
 }
