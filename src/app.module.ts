11310f0b7a525723f42672e1a48079d67e959201
Piero Muñante Avila <pmunanteavila@gmail.com>
2025-06-22 08:15:35 -0500

    Error corregido

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-
 import { PrismaModule } from './prisma/prisma.module';
 import { CargoModule } from './cargo/cargo.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       playground: true,
       path: '/graphql',
     }),
