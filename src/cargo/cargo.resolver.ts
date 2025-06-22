deb02a4b1a52a72a274b99f73463baef6e2bf73a
Piero Muñante Avila <pmunanteavila@gmail.com>
2025-06-21 23:08:57 -0500

    Se agrego servicios para muestra de datos de cargo

--- src/cargo/cargo.resolver.ts
+++ src/cargo/cargo.resolver.ts
@@ -1,4 +1,4 @@
-import { Int, Query, Resolver } from "@nestjs/graphql";
+import { Args, Float, Int, Query, Resolver } from "@nestjs/graphql";
 import { Cargo } from "./cargo.model";
 import { CargoService } from "./cargo.service";
 
@@ -14,5 +14,20 @@ export class CargoResolver {
     @Query(() => Int)
     async obtenerConteoCargos() {
         return this.cargoService.obtenerConteoCargos();
+    }
+
+    @Query(() => String, { nullable: true })
+    async obtenerCargoMejorPagado() {
+        return this.cargoService.obtenerCargoMejorPagado();
+    }
+
+    @Query(() => Float)
+    async obtenerMontoTotalHora(@Args('cargo', { nullable: true }) cargo?: string) {
+        return this.cargoService.obtenerMontoTotalHora(cargo);
+    }
+
+    @Query(() => Cargo, { nullable: true })
+    async obtenerCargoPorNombre(@Args('cargo') cargo: string) {
+        return this.cargoService.obtenerCargoPorNombre(cargo);
     }
 }
